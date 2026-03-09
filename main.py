90e1820ee56db04f8cb70b5a9460079e85ea0cd9
surendra-bolla <surendrabolla3@gmail.com>
2026-03-09 12:41:57 +0530

    first push

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from PIL import Image
+import io
+import torch
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+# Load BLIP model
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/caption")
+async def generate_caption(file: UploadFile = File(...)):
+    
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+    inputs = processor(image, return_tensors="pt")
+
+    out = model.generate(**inputs)
+
+    caption = processor.decode(out[0], skip_special_tokens=True)
+
+    return {"caption": caption}
